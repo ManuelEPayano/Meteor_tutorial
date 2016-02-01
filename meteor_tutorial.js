5a710efe4add765eb41cf36290a016537bdf4f2a
Manuel Payano <manuel.e.payano@gmail.com>
2016-02-01 18:05:25 +0000

    added helpers to the tutorial

--- meteor_tutorial.js
+++ meteor_tutorial.js
@@ -2,6 +2,14 @@ console.log("Hello world"); //Check that the WebApp is up.
 PlayersList = new Mongo.Collection('players'); // Starts the global variable PlayerList and the player collection
 if(Meteor.isClient){
     console.log("Hello Client");
+    Template.leaderboard.helpers({
+        'player':function(){
+            return "Some other text"  
+        },
+        'score':function(){
+            return "Some other helper"
+        }
+    });
 }
 if(Meteor.isServer){
     console.log("Hello Server");
